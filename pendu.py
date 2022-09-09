99fb42efeba38732add433ee3ee303904862427b
EnolaGae <95567476+EnolaGae@users.noreply.github.com>
2022-09-09 11:51:03 +0200

    Pendu qui marche
    Il manque juste la partie d'affichage des lettres (Clément stp)

--- pendu.py
+++ pendu.py
@@ -1,37 +1,33 @@
-import getpass
-from collections import Counter
+import os
 
-tentatives = 7
+tentatives = 0
+dico_mot = []
+# on demande le mot à montrer puis on le met en minuscule.
+solution = input("Enter le mot qui permetra de jouer au pendu: ")
+solution_minus= x=solution.lower()
+os.system("cls")
+dico_fautes = ["\n \n \n \n \n \n \n _|_","\n  | \n  |\n  | \n  |\n  |\n _|_ "," ________ \n  | \n  |\n  | \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  | \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /\ \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /|\ \n  |     |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /|\ \n  |     |\n  |    / \ \n _|_         Loose LOL"]
 
-# on demande le mot à montrer mais il n'apparait pas dans le terminal (pour ne pas tricher)
-solution = getpass.getpass(prompt = "Enter le mot qui permetra de jouer au pendu (il ne s'affichera pas ne vous inquiétez pas): ")
+# on va tester si les caractères correspondent à la liste acceptée.
+# ensuite on imprime le nombre de tirets bas correspondant au nombre de lettres dans le mot.
+for i in range(len(solution_minus)):
+    tiret_bas = []
+    dico_mot.append(solution_minus[i])
+    nb_caractere = len(solution)
+    nb_lettre = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","ç","é","è","â","ä","ù"]
+    if dico_mot[i] not in nb_lettre:
+        print("Veuillez entrer une lettre!")
+        solution = input('Entrez le mot à deviner: ')
+print(len(solution)*"_ ")
 
-
-# on va comparer le nombre de lettre du mot choisi (grace à counter) avec le nombre de caractère saisie plustot 
-# sa permtra d'exclure tous caractère spéciaux ou chiffre saisie par le mj
-nb_caractère = len(solution)
-c = Counter(solution)
-nb_lettre = c['a'] + c['z'] + c['e'] + c['r'] + c['t'] + c['y'] + c['u'] + c['i'] + c['o'] + c['p'] + c['q'] + c['s'] + c['d'] + c['f'] + c['g'] + c['h'] + c['j'] + c['k'] + c['l'] + c['m'] + c['w'] + c['x'] + c['c'] + c['v'] + c['b'] + c['n']
-if nb_caractère != nb_lettre:
-    print("Il ne faut pas d'espace, de tiré, de chiffre ou de nimporte quel autre caractère autre que des lettres ")
-    solution = getpass.getpass(prompt = 'Enter le mot qui permetra de jouer au pendu (il sera cacher ne vous inquiétez pas): ')
-
-
-# on montre maintenant le nombre de lettre à trouver
-print("Le mot à trouver est le suivant")
-affichage = ""
-for l in solution:
-    affichage = affichage + "_ "
-print(affichage)
-
-
-# le joueur commence à jouer et on lui dit si il a vrai ou si il a faux en lui redonnant son nombre de tentatives
-proposition = input("proposez une lettre : ")
-lettres_trouvees = ""
-if proposition in solution:
-    lettres_trouvees = lettres_trouvees + proposition
-    print("Bravo !")
-else:
-    tentatives = tentatives - 1
-    print("Rater il vous reste ", tentatives, " tentatives")
- 
+# le joueur commence à deviner et on lui dit si il a vrai ou si il a faux en lui redonnant son nombre de tentatives
+while tentatives < 7:
+    proposition = input("Proposez une lettre : ")
+    lettres_trouvees = ""
+    if proposition in solution:
+        lettres_trouvees = lettres_trouvees + proposition
+        print("Bonne lettre!")
+    else:
+        print("Raté, il vous reste ", 7 - tentatives, " tentatives")
+        print(dico_fautes[tentatives])
+        tentatives = tentatives + 1
--- test_lettres.py
+++ test_lettres.py
@@ -1,15 +1,15 @@
 tentatives = 7
 dico_mot = []
-# on demande le mot à montrer mais il n'apparait pas dans le terminal (pour ne pas tricher)
-solution = input("Enter le mot qui permetra de jouer au pendu (il ne s'affichera pas ne vous inquiétez pas): ")
+# on demande le mot à montrer puis on le mets en minuscule.
+def demande_mot():
+    solution = input("Enter le mot qui permetra de jouer au pendu (il ne s'affichera pas ne vous inquiétez pas): ")
+    solution_minus= x=solution.lower()
 
-modif_solution= x=solution.lower()
-
-# on va comparer le nombre de lettre du mot choisi (grace à counter) avec le nombre de caractère saisie plustot 
-# sa permtra d'exclure tous caractère spéciaux ou chiffre saisie par le mj
-for i in range(len(modif_solution)):
+# on va tester si les caractères correspondent à la liste acceptée.
+# ensuite on imprime le nombre de tirets bas correspondant au nombre de lettres dans le mot.
+for i in range(len(solution_minus)):
     tiret_bas = []
-    dico_mot.append(modif_solution[i])
+    dico_mot.append(solution_minus[i])
     nb_caractere = len(solution)
     nb_lettre = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","ç","é","è","â","ä","ù"]
     if dico_mot[i] not in nb_lettre:
