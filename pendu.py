2efa7b506fae72d6cbf062a6691ca8c40922f26c
EnolaGae <95567476+EnolaGae@users.noreply.github.com>
2022-09-09 13:39:39 +0200

    Update pendu.py

--- pendu.py
+++ pendu.py
@@ -4,21 +4,22 @@ tentatives = 0
 dico_mot = []
 # on demande le mot à montrer puis on le met en minuscule.
 solution = input("Enter le mot qui permetra de jouer au pendu: ")
-solution_minus= x=solution.lower()
+solution_minuscule= x=solution.lower()
 os.system("cls")
 dico_fautes = ["\n \n \n \n \n \n \n _|_","\n  | \n  |\n  | \n  |\n  |\n _|_ "," ________ \n  | \n  |\n  | \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  | \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /\ \n  |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /|\ \n  |     |\n  |\n _|_ "," ________ \n  |     | \n  |     0\n  |    /|\ \n  |     |\n  |    / \ \n _|_         Loose LOL"]
 
 # on va tester si les caractères correspondent à la liste acceptée.
 # ensuite on imprime le nombre de tirets bas correspondant au nombre de lettres dans le mot.
-for i in range(len(solution_minus)):
+for i in range(len(solution_minuscule)):
     tiret_bas = []
-    dico_mot.append(solution_minus[i])
+    dico_mot.append(solution_minuscule[i])
+    tiret_bas.append("_")
     nb_caractere = len(solution)
     nb_lettre = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","ç","é","è","â","ä","ù"]
     if dico_mot[i] not in nb_lettre:
         print("Veuillez entrer une lettre!")
         solution = input('Entrez le mot à deviner: ')
-print(len(solution)*"_ ")
+print(len(solution_minuscule)*"_ ")
 
 # le joueur commence à deviner et on lui dit si il a vrai ou si il a faux en lui redonnant son nombre de tentatives
 while tentatives < 7:
@@ -31,3 +32,4 @@ while tentatives < 7:
         print("Raté, il vous reste ", 7 - tentatives, " tentatives")
         print(dico_fautes[tentatives])
         tentatives = tentatives + 1
+print(tiret_bas, "tiret_bas")
